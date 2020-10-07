7fc531137a3240cd502f1a6b531ffef90679cd84
Vykintas Baltrušaitis <vykintas@baltr.lt>
2020-10-07 15:08:48 +0300

    Use records

--- snake.hs
+++ snake.hs
@@ -1,3 +1,12 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# OPTIONS_GHC -fplugin=RecordDotPreprocessor #-}
+
 import Brick.AttrMap
 import Brick.BChan
 import qualified Brick.Main as M
@@ -10,18 +19,33 @@ import Control.Monad
 import Graphics.Vty
 import qualified Graphics.Vty as V
 import System.Random
+import Prelude hiding (Left, Right)
 
-data SPosition = SPosition Int Int deriving (Show, Eq)
+data Position = Position
+  { positionX :: Int,
+    positionY :: Int
+  }
+  deriving (Show, Eq)
 
-data SDirection = SUp | SRight | SDown | SLeft deriving (Show, Eq)
+data Direction = Up | Right | Down | Left deriving (Show, Eq)
 
-data SSnake = SSnake [SPosition] SDirection deriving (Show, Eq)
+data Snake = Snake
+  { snakeBody :: [Position],
+    snakeDirection :: Direction
+  }
+  deriving (Show, Eq)
 
-data SStatus = SStarted | SInProgress | SOver deriving (Show, Eq)
+data Status = Started | InProgress | GameOver deriving (Show, Eq)
 
-data SState = SState SSnake SStatus [SPosition] StdGen deriving (Show)
+data State = State
+  { stateSnake :: Snake,
+    stateStatus :: Status,
+    stateFood :: [Position],
+    stateRng :: StdGen
+  }
+  deriving (Show)
 
-data STimeUnitEvent = STimeUnitEvent
+data TimeUnitEvent = TimeUnitEvent
 
 lowerGridBound :: Int
 lowerGridBound = 1
@@ -29,17 +53,23 @@ lowerGridBound = 1
 upperGridBound :: Int
 upperGridBound = 21
 
-snakeMatrix :: SState -> [String]
-snakeMatrix (SState (SSnake body _) status food _) = case status of
-  SStarted -> grid
-  SInProgress -> grid
-  SOver -> mergedGrid
+snakeMatrix :: State -> [String]
+snakeMatrix state = case state.stateStatus of
+  Started -> grid
+  InProgress -> grid
+  GameOver -> mergedGrid
   where
     mergedGrid = zipWith mergedLine loserGrid grid
-    mergedLine loserL snakeL = zipWith (\a b -> if a == ' ' then b else a) loserL snakeL
+    mergedLine loserL snake = zipWith (\a b -> if a == ' ' then b else a) loserL snake
     loserGrid = reverse [loserLine y | y <- [lowerGridBound .. upperGridBound]]
     grid = reverse [line y | y <- [lowerGridBound .. upperGridBound]]
-    cell x y = if SPosition x y `elem` body then "()" else if SPosition x y `elem` food then "[]" else "  "
+    cell x y =
+      if Position x y `elem` (state.stateSnake.snakeBody)
+        then "()"
+        else
+          if Position x y `elem` (stateFood state)
+            then "[]"
+            else "  "
     line y = concat [cell x y | x <- [lowerGridBound .. upperGridBound]]
     loserLine y =
       if y `mod` 2 == 0
@@ -47,23 +77,23 @@ snakeMatrix (SState (SSnake body _) status food _) = case status of
         else take lineLength $ drop y $ concat $ repeat "YOU LOSE  "
     lineLength = (upperGridBound - lowerGridBound + 1) * 2
 
-drawUi :: SState -> [T.Widget ()]
+drawUi :: State -> [T.Widget ()]
 drawUi state = [C.center $ B.border $ vBox (fmap str (snakeMatrix state))]
 
-updateDirection :: SSnake -> SDirection -> SSnake
-updateDirection (SSnake body oldDirection) direction
-  | (direction == SLeft || direction == SRight) && vertical = SSnake body direction
-  | (direction == SUp || direction == SDown) && horizontal = SSnake body direction
-  | otherwise = SSnake body oldDirection
+updateDirection :: Snake -> Direction -> Snake
+updateDirection (Snake body oldDirection) direction
+  | (direction == Left || direction == Right) && vertical = Snake body direction
+  | (direction == Up || direction == Down) && horizontal = Snake body direction
+  | otherwise = Snake body oldDirection
   where
     horizontal = case body of
-      SPosition _ y : SPosition _ y' : _ -> y == y'
+      Position _ y : Position _ y' : _ -> y == y'
       _ -> True
     vertical = case body of
-      SPosition x _ : SPosition x' _ : _ -> x == x'
+      Position x _ : Position x' _ : _ -> x == x'
       _ -> True
 
-randomFood :: StdGen -> [SPosition] -> ([SPosition], StdGen)
+randomFood :: StdGen -> [Position] -> ([Position], StdGen)
 randomFood rng body =
   if emptyCells == []
     then ([], rng)
@@ -71,31 +101,31 @@ randomFood rng body =
       (idx, rng') -> ([emptyCells !! idx], rng')
   where
     emptyCells =
-      [ SPosition x y
+      [ Position x y
         | x <- [lowerGridBound .. upperGridBound],
           y <- [lowerGridBound .. upperGridBound],
-          not $ SPosition x y `elem` body
+          not $ Position x y `elem` body
       ]
 
-moveSnake :: SState -> SState
-moveSnake (SState (SSnake body direction) status food rng) =
-  SState (SSnake newBody direction) newStatus newFood newRng
+moveSnake :: State -> State
+moveSnake (State (Snake body direction) status food rng) =
+  State (Snake newBody direction) newStatus newFood newRng
   where
-    SPosition currentHeadX currentHeadY = head body
+    Position currentHeadX currentHeadY = head body
     newHead = case direction of
-      SUp -> SPosition currentHeadX (currentHeadY + 1)
-      SRight -> SPosition (currentHeadX + 1) currentHeadY
-      SDown -> SPosition currentHeadX (currentHeadY - 1)
-      SLeft -> SPosition (currentHeadX - 1) currentHeadY
+      Up -> Position currentHeadX (currentHeadY + 1)
+      Right -> Position (currentHeadX + 1) currentHeadY
+      Down -> Position currentHeadX (currentHeadY - 1)
+      Left -> Position (currentHeadX - 1) currentHeadY
     willBeOutOfBounds = case newHead of
-      SPosition newX newY ->
+      Position newX newY ->
         min newX newY < lowerGridBound
           || max newX newY > upperGridBound
           || newHead `elem` tail newBody
     willEatFood = newHead `elem` food
-    newStatus = if willBeOutOfBounds then SOver else status
+    newStatus = if willBeOutOfBounds then GameOver else status
     newBody =
-      if status == SOver
+      if status == GameOver
         then body
         else
           if willEatFood
@@ -103,20 +133,20 @@ moveSnake (SState (SSnake body direction) status food rng) =
             else newHead : (reverse $ tail $ reverse body)
     (newFood, newRng) = if willEatFood then randomFood rng newBody else (food, rng)
 
-appEvent :: SState -> T.BrickEvent () STimeUnitEvent -> T.EventM () (T.Next SState)
-appEvent state@(SState snake status food rng) (T.VtyEvent (V.EvKey keyAction [])) = case keyAction of
-  V.KUp -> action SUp
-  V.KRight -> action SRight
-  V.KDown -> action SDown
-  V.KLeft -> action SLeft
+appEvent :: State -> T.BrickEvent () TimeUnitEvent -> T.EventM () (T.Next State)
+appEvent state@(State snake status food rng) (T.VtyEvent (V.EvKey keyAction [])) = case keyAction of
+  V.KUp -> action Up
+  V.KRight -> action Right
+  V.KDown -> action Down
+  V.KLeft -> action Left
   V.KEsc -> M.halt state
   _ -> M.continue state
   where
-    action newDirection = M.continue $ SState (updateDirection snake newDirection) status food rng
-appEvent state (T.AppEvent STimeUnitEvent) = M.continue $ moveSnake state
+    action newDirection = M.continue $ State (updateDirection snake newDirection) status food rng
+appEvent state (T.AppEvent TimeUnitEvent) = M.continue $ moveSnake state
 appEvent state _ = M.continue state
 
-app :: M.App SState STimeUnitEvent ()
+app :: M.App State TimeUnitEvent ()
 app =
   M.App
     { M.appDraw = drawUi,
@@ -126,9 +156,9 @@ app =
       M.appChooseCursor = M.neverShowCursor
     }
 
-pingSnake :: Brick.BChan.BChan STimeUnitEvent -> IO b
+pingSnake :: Brick.BChan.BChan TimeUnitEvent -> IO b
 pingSnake chan = forever $ do
-  _ <- Brick.BChan.writeBChanNonBlocking chan STimeUnitEvent
+  _ <- Brick.BChan.writeBChanNonBlocking chan TimeUnitEvent
   threadDelay 100000
   return ()
 
@@ -139,9 +169,9 @@ main = do
   initialVty <- buildVty
   seed <- randomIO :: IO Int
   let rng = mkStdGen seed
-      body = [SPosition x 10 | x <- [12, 11 .. 10]]
+      body = [Position x 10 | x <- [12, 11 .. 10]]
       (food, _) = randomFood rng body
-      initialState = SState (SSnake body SRight) SStarted food rng
+      initialState = State (Snake body Right) Started food rng
   _ <- forkIO $ pingSnake eventChan
   _ <- M.customMain initialVty buildVty (Just eventChan) app initialState
   return ()
